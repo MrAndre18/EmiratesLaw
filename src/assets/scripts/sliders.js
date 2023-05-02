03c1e1dfda9519ae715a2241f251edc502a6f041
MrAndre <Andrew09200@mail.ru>
2023-05-02 18:48:13 +0300

    Update

--- src/assets/scripts/sliders.js
+++ src/assets/scripts/sliders.js
@@ -1,23 +1,77 @@
 import Swiper from 'swiper/bundle'
 
-const mainTopSlider = new Swiper('.title-block__slider', {
-  // Optional parameters
-  direction: 'horizontal',
-  slideClass: 'title-block__content',
-  wrapperClass: 'title-block__slider-wrapper',
-  loop: true,
-  speed: 400,
-  effect: 'fade',
-  slidesPerView: 1,
+$(() => {
+  const easing = 'cubic-bezier(0.45, 0, 0.55, 1)'
 
-  // If we need pagination
-  pagination: {
-    el: '.title-block__pages-number',
-  },
+  // Верхний слайдер главной
+  if ($('.title-block__body').length) {
+    const delay = 10000,
+          slider = $('.title-block__body'),
+          loading = $(slider).find('.title-block__pages-loading')
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.title-block__navigation-next',
-    prevEl: '.title-block__navigation-prev',
-  },
+    const stopAnimation = () => {
+      $(loading).stop()
+      $(loading).css('width', 0)
+    }
+
+    const startLoading = () => {
+      $(loading).animate({
+        width: "100%"
+      },
+      {
+        duration: delay,
+        easing: "linear"
+      })
+    }
+
+    const mainTopSlider = new Swiper('.title-block__body', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      speed: 400,
+      spaceBetween: 0,
+      effect: "fade",
+      fadeEffect: {
+        crossFade: true
+      },
+      simulateTouch: false,
+      slidesPerView: 1,
+      autoplay: {
+        delay: delay,
+      },
+    
+      // If we need pagination
+      pagination: {
+        el: '.title-block__pages-pagination',
+        clickable: false,
+        bulletClass: 'title-block__pages-item',
+        bulletActiveClass: 'active',
+        renderBullet: (index, className) => {
+          return `<div class="${className}">
+                    <p class="title-block__pages-number">0${(index + 1)}</p>
+                    <p class="title-block__pages-text">
+                      
+                      ${console.log(Swiper)}
+                    </p>
+                  </div>`
+        }
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.title-block__navigation-next',
+        prevEl: '.title-block__navigation-prev',
+      },
+
+      on: {
+        init: () => {
+          startLoading()
+        },
+        slideChange: () => {
+          stopAnimation()
+          startLoading()
+        },
+      },
+    })
+  }
 })
