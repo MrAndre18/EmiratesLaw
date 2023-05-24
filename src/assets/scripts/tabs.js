ac9375844b3733449735ca67d43e58873bdbf4f7
MrAndre <Andrew09200@mail.ru>
2023-05-25 02:16:25 +0300

    Update

--- src/assets/scripts/tabs.js
+++ src/assets/scripts/tabs.js
@@ -1,10 +1,36 @@
 $(() => {
-  const tabs = $('[data-type=js-tabs]')
-  
-  if ($(tabs).length) {
-    $(tabs).each(function (index, tab) {
+  const tabs = $('[data-type=js-tabs]'),
+        pages = $('.info__page')
+
+  const showPage = (id) => {
+    const curPage = $(`#${id}`)
+    
+    $(pages).removeClass('active')
+    $(curPage).addClass('active')
+  }
+
+  const setPage = (params) => {
+    const linkUrlParams = new URLSearchParams(params),
+          urlParams = new URLSearchParams(window.location.search),
+          curPage = urlParams.get('page'),
+          newPage = linkUrlParams.get('page'),
+          pathname = window.location.href.split('?')[0],
+          hash = window.location.href.split('#')[1]
+          
+    if (curPage != newPage) {
+      urlParams.set('page', newPage)
+      const newUrl = `${pathname}` + `${urlParams.toString() ? '?' + urlParams.toString() : ''}` + `${hash ? '#' + hash : ''}`
+
+      history.pushState({}, '', newUrl)
+      showPage(newPage)
+    }
+  }
+
+  const setTabs = () => {
+    $(tabs).each((index, tab) => {
       const items = $(tab).find('.tab'),
-            background = $(tab).find('.tabs-bg')
+            background = $(tab).find('.tabs-bg'),
+            currentItem = $(tab).find('.current')[0]
 
       let currentItemPosition = $(tab).find('.current').offset().left,
           currentItemWidth = $(tab).find('.current').width()
@@ -25,25 +51,82 @@ $(() => {
         setBgProperties(currentItemPosition, currentItemWidth)
       }
 
-      setBgProperties(currentItemPosition, currentItemWidth)
+      const initialization = () => {
+        $(items).each((index, element) => {
+          const linkUrlParams = new URLSearchParams($(element).find('a')[0].search),
+                urlParams = new URLSearchParams(window.location.search),
+                curPage = urlParams.get('page'),
+                newPage = linkUrlParams.get('page')
+  
+          if (!curPage) {
+            setBgProperties(currentItemPosition, currentItemWidth)
+            setPage($(currentItem).find('a')[0].search)
+
+            return
+          }
+
+          if (curPage == newPage) {
+            $(element).click()
+            showPage(newPage)
+          }
+        })
+      }
+
+      //setBgProperties(currentItemPosition, currentItemWidth)
+      //setPage()
 
       $(items).on("mouseover", e => {
         const newPosition = $(e.currentTarget).offset().left,
-              newWidth = $(e.currentTarget).width()
+              newWidth = $(e.currentTarget).width(),
+              curItem = $(tab).find('.current')[0]
 
         setBgProperties(newPosition, newWidth)
+        $(e.currentTarget).addClass('focus')
+
+        if ($(e.currentTarget)[0] !== curItem) {
+          $(curItem).addClass('focus-out')
+        } else {
+          $(curItem).removeClass('focus-out')
+        }
       })
 
       $(items).on("mouseout", e => {
         setBgProperties(currentItemPosition, currentItemWidth)
+        $(e.currentTarget).removeClass('focus')
       })
 
       $(items).on("click", e => {
+        e.preventDefault()
+
         clearCurrent()
         $(e.currentTarget).addClass('current')
+        $(e.currentTarget).addClass('focus')
+        $(e.currentTarget).removeClass('focus-out')
         setCurrent()
+
+        setPage($(e.currentTarget).find('a')[0].search)
       })
-    });
-    
+
+      $(tab).on('mouseleave', e => {
+        const curItem = $(tab).find('.current')[0]
+
+        $(curItem).removeClass('focus-out')
+      })
+
+      // $(items).each((index, element) => {
+      //   const linkUrlParams = new URLSearchParams($(element).find('a')[0].search),
+      //         urlParams = new URLSearchParams(window.location.search),
+      //         curPage = urlParams.get('page'),
+      //         newPage = linkUrlParams.get('page')
+
+      //   if (curPage == newPage) {
+      //     $(element).click()
+      //     showPage(newPage)
+      //   }
+      // })
+      initialization()
+    })
   }
+  
+  $(tabs).length ? setTabs() : null
 })
