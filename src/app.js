c5b19507c851100bb6edf211d93c69785966334b
MrAndre <Andrew09200@mail.ru>
2023-05-26 18:06:24 +0300

    Update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import { isWebp } from './assets/scripts/functions.js'
 isWebp()
 
-import "./assets/scripts/loader.js"
-
 import "./assets/scripts/convert-svg.js"
 import "./assets/scripts/scroll.js"
 import "./assets/scripts/drop-down.js"
@@ -19,3 +17,5 @@ import "./assets/scripts/tabs.js"
 
 
 import "./assets/scripts/sliders.js"
+
+import "./assets/scripts/loader.js"
--- src/assets/scripts/loader.js
+++ src/assets/scripts/loader.js
@@ -1,7 +1,29 @@
-const content = $('.content'),
-      loader = $('.loader')
+const loader = $('.loader'),
+      loaderProgress = loader.find('.progress-ring'),
+      circle = loaderProgress.find('.progress-ring__circle'),
+      radius = circle.attr('r'),
+      circumference = 2 * Math.PI * radius,
+      loaderDuration = 1500;
 
 $(() => {
-  $(content).addClass('visible')
-  $(loader).fadeOut("slow")
+  circle.css("strokeDasharray", `${circumference} ${circumference}`)
+  circle.css("strokeDashoffset", circumference)
+  circle.css(
+    "transition",
+    `stroke-dashoffset ${loaderDuration}ms cubic-bezier(0.65, 0, 0.35, 1)`
+  )
+  circle.css("display", "block")
+
+  function preloaderClose() {
+    circle.css("strokeDashoffset", 0)
+
+    setTimeout(() => {
+      loader.addClass("hidden")
+      $("body").css("overflow", "auto")
+    }, loaderDuration)
+  }
+
+  setTimeout(() => {
+    preloaderClose();
+  }, 1)
 })
