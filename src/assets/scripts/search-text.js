05fdd885eaffef08d52b7a90c1ae6b563f6dc94a
MrAndre <Andrew09200@mail.ru>
2023-05-31 17:32:19 +0300

    Update

--- src/assets/scripts/search-text.js
+++ src/assets/scripts/search-text.js
@@ -1,12 +1,86 @@
+//import Mark from '../../../node_modules/mark.js/dist/mark.min.js'
+import Mark from "mark.js"
+
 export const searchText = pageId => {
   const searchBtnBlock = $(`#${pageId}`).find('.laws-detail__search'),
-      searchBtn = $(searchBtnBlock).find('.laws-detail__search-btn'),
-      searchInput = $(searchBtnBlock).find('.laws-detail__search-input'),
-      searchPrevResultBtn = $(searchBtnBlock).find('.laws-detail__search-navigation-prev'),
-      searchNextResultBtn = $(searchBtnBlock).find('.laws-detail__search-navigation-next'),
-      searchCloseBtn = $(searchBtnBlock).find('.laws-detail__search-close')
+        searchBtn = $(searchBtnBlock).find('.laws-detail__search-btn'),
+        searchInput = $(searchBtnBlock).find('.laws-detail__search-input'),
+        searchCloseBtn = $(searchBtnBlock).find('.laws-detail__search-close'),
+        searchResultBtns = {
+          block: $(searchBtnBlock).find('.laws-detail__search-navigation'),
+          prev: $(searchBtnBlock).find('.laws-detail__search-navigation-prev'),
+          next: $(searchBtnBlock).find('.laws-detail__search-navigation-next')
+        },
+        searchCountBlock = {
+          block: $(searchBtnBlock).find('.laws-detail__search-result'),
+          current: $(searchBtnBlock).find('.laws-detail__search-result-current'),
+          total: $(searchBtnBlock).find('.laws-detail__search-result-total')
+        }
+  
+  const pageContent = $(`#${pageId}`).find('.laws-detail__content')[0]
+
+  let searchResultArr = [],
+      resultCount = 0,
+      currentResultNumber = 1
+
+  const setTotalCount = count => {
+    resultCount = count
+
+    if (count == 0) {
+      $(searchResultBtns.block).removeClass('active')
+    } else {
+      $(searchResultBtns.block).addClass('active')
+    }
+
+    searchCountBlock.total.text(count)
+  }
+
+  const selectHighlight = () => {
+    const curResult = $(pageContent).find(`[data-search-number=${currentResultNumber}]`)
+
+    $(curResult).addClass('selected')
+
+    searchCountBlock.current.text(currentResultNumber)
+  }
+
+  const scrollToCurrentResult = () => {
+    const selectedResult = $(pageContent).find('.highlight.selected')
+
+    $('html, body').animate({
+      scrollTop: $(selectedResult).offset().top - 130
+    }, 400)
+  }
+
+  const setCurrentResult = () => {
+    $(searchResultBtns.prev).on('click', () => {
+      if (currentResultNumber == 1)
+        currentResultNumber = resultCount
+      else
+        currentResultNumber--
+      
+      $('.highlight').removeClass('selected')
+
+      selectHighlight()
+      scrollToCurrentResult()
+    })
+
+    $(searchResultBtns.next).on('click', () => {
+      if (currentResultNumber == resultCount)
+        currentResultNumber = 1
+      else
+        currentResultNumber++
+      
+      $('.highlight').removeClass('selected')
+
+      selectHighlight()
+      scrollToCurrentResult()
+    })
+  }
 
   const clearInput = () => {
+    const instance = new Mark(pageContent)
+    
+    instance.unmark()
     $(searchInput).val('')
   }
 
@@ -16,14 +90,56 @@ export const searchText = pageId => {
   }
 
   const disactivateSearchInput = () => {
+    $(searchCountBlock.block).removeClass('active')
+    $(searchResultBtns.block).removeClass('active')
     $(searchBtnBlock).removeClass('active')
     $(searchInput).prop('disabled', true)
     clearInput()
   }
 
+  const highlightSearch = text => {
+    const instance = new Mark(pageContent)
+
+    instance.unmark()
+    instance.mark(text, {
+      "element": "mark",
+      "className": "highlight",
+      "each": (node) => {
+        searchResultArr.push(node)
+      },
+      "done": (counter) => {
+        setTotalCount(counter)
+      },
+    })
+
+    $(searchResultArr).each((index, element) => {
+      $(element).attr('data-search-number', index + 1)
+      $(element).removeClass('selected')
+    })
+
+    if (resultCount) {
+      selectHighlight()
+      scrollToCurrentResult()
+    }
+
+    setCurrentResult()
+  }
+
   disactivateSearchInput()
 
-  $(searchBtn).on('click', activateSearchInput)
+  $(searchBtn).on('click', e => {
+    activateSearchInput()
+
+    searchResultArr = []
+    resultCount = 0
+
+    if ($(searchInput).val())
+      $(searchCountBlock.block).addClass('active')
+    else
+      $(searchCountBlock.block).removeClass('active')
+
+    highlightSearch($(searchInput).val())
+  })
 
   $(searchCloseBtn).on('click', disactivateSearchInput)
 }
