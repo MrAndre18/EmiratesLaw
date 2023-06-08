c606934a52ed26f5f0d45a982ad318dfbc3a2870
MrAndre <Andrew09200@mail.ru>
2023-06-08 18:35:05 +0300

    Update

--- src/assets/scripts/modals.js
+++ src/assets/scripts/modals.js
@@ -1,37 +1,37 @@
 if ($("[data-type=js-open-btn]").length) {
-  const openBtns = $("[data-type=js-open-btn]");
+  const openBtns = $("[data-type=js-open-btn]")
 
   $(openBtns).each(function (index, btn) {
     $(btn).on("click", (e) => {
-      e.preventDefault();
+      e.preventDefault()
 
       const modalType = $(btn).data("modal").replace("-btn", ""),
-        modal = $(`[data-modal=${modalType}]`);
+        modal = $(`[data-modal=${modalType}]`)
 
-      if (!$(modal).length) return;
+      if (!$(modal).length) return
 
-      $(modal).addClass("active");
-      $(btn).addClass("active");
-    });
-  });
+      $(modal).addClass("active")
+      $(btn).addClass("active")
+    })
+  })
 }
 
 if (
   $("[data-type=js-modal]").length &&
   $("[data-type=js-close-btn]").length
 ) {
-  const closeBtns = $("[data-type=js-close-btn]");
+  const closeBtns = $("[data-type=js-close-btn]")
 
   $(closeBtns).each(function (index, btn) {
     $(btn).on("click", () => {
-      const parentModal = btn.closest("[data-type=js-modal]");
+      const parentModal = btn.closest("[data-type=js-modal]")
 
-      if (!$(parentModal).length) return;
+      if (!$(parentModal).length) return
 
-      $(parentModal).removeClass("active");
+      $(parentModal).removeClass("active")
 
-      const modalBtns = $(`[data-modal=${$(parentModal).data("modal")}-btn]`);
-      $(modalBtns).removeClass("active");
+      const modalBtns = $(`[data-modal=${$(parentModal).data("modal")}-btn]`)
+      $(modalBtns).removeClass("active")
     });
   });
 }
--- src/assets/scripts/pages-pagination.js
+++ src/assets/scripts/pages-pagination.js
@@ -8,92 +8,66 @@ $(() => {
   $(paginationControls).each((index, pagination) => {
     const paginationContent = $(`[data-pagination-content=${$(pagination).data('paginationTarget')}]`),
           paginationContentItems = $(paginationContent).find('[data-type=pagination-item]').toArray(),
-          pageSize = $(paginationContent).data('pageSize')
+          pageSize = $(paginationContent).data('pageSize'),
+          background = $(pagination).find('.pages-pagination__bg')
+
+    const eventsFiltersBlock = $(paginationContent).find('.events__content-filters'),
+          eventsTitleBlock = $(paginationContent).find('.events__content-title')
+
+    const setCurrentPosition = () => {
+      const items = $(pagination).find('.pages-pagination__item')
+
+      let currentItemPosition = $(pagination).find('.pages-pagination__item-current').offset().left
+
+      $(background).offset({ left: currentItemPosition })
+
+      $(items).on("mouseover", e => {
+        currentItemPosition = $(pagination).find('.pages-pagination__item-current').offset().left
+        const newPosition = $(e.currentTarget).offset().left
+  
+        $(background).offset({ left: newPosition})
+      })
+  
+      $(items).on("mouseout", e => {
+        $(background).offset({ left: currentItemPosition })
+      })
+    }
 
     $(pagination).pagination({
       dataSource: paginationContentItems,
       pageSize: pageSize,
       hideOnlyOnePage: true,
+
       ulClassName: 'pages-pagination__items',
       pageClassName: 'pages-pagination__item',
       prevClassName: 'pages-pagination__button-prev',
       nextClassName: 'pages-pagination__button-next',
       activeClassName: 'pages-pagination__item-current',
       disableClassName: 'pages-pagination__button-disabled',
+
       prevText: '<img class="pages-pagination__button-img svg", src="assets/images/svg/pagination-arrow.svg", alt="Prev"></img>',
       nextText: '<img class="pages-pagination__button-img svg", src="assets/images/svg/pagination-arrow.svg", alt="Prev"></img>',
       
       callback: (data, pagination) => {
         $(paginationContent).html(data)
+        if (eventsFiltersBlock.length && eventsTitleBlock.length) {
+          $(paginationContent).prepend($(eventsTitleBlock)[0])
+          $(paginationContent).prepend($(eventsFiltersBlock)[0])
+        }
+
+        $(paginationContent).fadeOut(0)
+        $(paginationContent).fadeIn(400)
+      },
+
+      afterPaging: () => {
+        setCurrentPosition()
+
+        if ($(window).scrollTop() > $(paginationContent).offset().top - 140) {
+          $('html, body').animate({
+            scrollTop: $(paginationContent).offset().top - 130
+          }, 600)
+        }
       },
     })
   })
-
-
-
-
-  // if($('.pages-pagination').length) {
-  //   const paginationBlocks = $('.pages-pagination')
-    
-  //   $(paginationBlocks).each(function (index, pagination) {
-  //     const items = $(pagination).find('.pages-pagination__item'),
-  //           background = $(pagination).find('.pages-pagination__items-bg'),
-  //           btnPrev = $(pagination).find('.pages-pagination__button-prev'),
-  //           btnNext = $(pagination).find('.pages-pagination__button-next')
-
-  //     let currentItemPosition = $(pagination).find('.pages-pagination__item-current').offset().left
-
-  //     $(background).offset({ left: currentItemPosition })
-
-  //     const clearCurrent = () => {
-  //       $(items).removeClass('pages-pagination__item-current')
-  //     }
-
-  //     const setCurrentPosition = () => {
-  //       currentItemPosition = $(pagination).find('.pages-pagination__item-current').offset().left
-  //       $(background).offset({ left: currentItemPosition })
-  //     }
-
-  //     const setPrevElement = () => {
-  //       const currentElement = $(pagination).find('.pages-pagination__item-current'),
-  //             prevElement = $(currentElement).prev('.pages-pagination__item')
-
-  //       if ($(prevElement).length) {
-  //         clearCurrent()
-  //         $(prevElement).addClass('pages-pagination__item-current')
-  //         setCurrentPosition()
-  //       }
-  //     }
-
-  //     const setNextElement = () => {
-  //       const currentElement = $(pagination).find('.pages-pagination__item-current'),
-  //             nextElement = $(currentElement).next('.pages-pagination__item')
-
-  //       if ($(nextElement).length) {
-  //         clearCurrent()
-  //         $(nextElement).addClass('pages-pagination__item-current')
-  //         setCurrentPosition()
-  //       }
-  //     }
-      
-  //     $(items).on("mouseover", e => {
-  //       const newPosition = $(e.currentTarget).offset().left
-
-  //       $(background).offset({ left: newPosition})
-  //     })
-
-  //     $(items).on("mouseout", e => {
-  //       $(background).offset({ left: currentItemPosition })
-  //     })
-
-  //     $(items).on("click", e => {
-  //       clearCurrent()
-  //       $(e.currentTarget).addClass('pages-pagination__item-current')
-  //       setCurrentPosition()
-  //     })
-
-  //     $(btnPrev).on("click", setPrevElement)
-  //     $(btnNext).on("click", setNextElement)
-  //   });
-  // }
 })
